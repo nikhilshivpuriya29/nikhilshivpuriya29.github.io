367bf33af606f5d9c4eaee6878805d590370868a
Nikhil Shivpuriya <nikhilshivpuriya29@gmail.com>
2025-01-23 00:00:38 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base: '/nikhilshivpuriya29.github.io/', // This should match your repository name
+  base: '/', // Updated to match the root domain
 });
